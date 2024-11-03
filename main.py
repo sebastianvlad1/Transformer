887bd735fce03e33fba2b1ebef9a93a7c301f66c
BRINZAS SEBASTIAN-VLAD <brinzassebastian-vlad@BRINZASs-MacBook-Air.local>
2024-11-03 11:50:23 +0200

    Train the model

--- main.py
+++ main.py
@@ -11,19 +11,52 @@ def main():
     block_size = 8
     batch_size = 4
 
-    xb, yb = get_batch(train_data, batch_size, block_size)
-    # print(xb)
-    # for batch in range(batch_size):
-    #     for block in range(block_size):
-    #         context = xb[batch, :block+1]
-    #         target = yb[batch, block]
-    #         print(f"When the input is {context}, the output is {target}")
-
     model = BigramLanguageModel(len(vocabulary))
-    out, loss = model(xb,yb)
-    print(loss)
 
     print(detokenize(model.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist(), itos))
+
+    @torch.no_grad()
+    def estimate_loss(eval_iters):
+        out = {}
+        model.eval()
+
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            x, y = get_batch(train_data, batch_size, block_size)
+            logits, loss = model(x, y)
+            losses[k] = loss.item()
+        train_losses = losses.mean()
+
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            x, y = get_batch(val_data, batch_size, block_size)
+            logits, loss = model(x, y)
+            losses[k] = loss.item()
+        val_losses = losses.mean()
+
+        model.train()
+
+        return train_losses, val_losses
+
+    eval_iters = 10000
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+    print(detokenize(model.generate(idx=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=400)[0].tolist(), itos))
+
+    for iter in range(eval_iters):
+
+        if iter % 1000 == 0:
+            train_losses, val_losses = estimate_loss(eval_iters)
+            print(f"Step {iter}: train loss: {train_losses:.4f}, val loss: {val_losses:.4f}")
+
+        xb,yb = get_batch(train_data, batch_size, block_size)
+
+        logits, loss = model(xb,yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    print(detokenize(model.generate(idx=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=400)[0].tolist(), itos))
 
 
 if __name__ == "__main__":
